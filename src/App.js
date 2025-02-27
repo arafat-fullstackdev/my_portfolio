8377f3787e487bf443d61b7c7a152b77924dac01
Arafat Patwary <farabipsk@gmail.com>
2025-02-27 10:05:09 +0600

    Implement Router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-//import './App.css';
-import Home from './components/Home';
-import NavBar from './components/Navbar';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import "./App.css";
+import Home from "./components/Home";
+import NavBar from "./components/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./components/About";
+import Project from "./components/Project";
+import Blog from "./components/Blog";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <NavBar/>
-        <Home/>
-
-      </header>
-    </div>
+    <Router>
+      <NavBar />  {/* Navbar should be outside Routes */}
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
